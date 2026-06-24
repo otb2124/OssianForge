fd5743296a6bb82c2ec24a5762a5d2d3a9b04507
orest.brukhal <otb2124@g.rit.edu>
2026-06-25 01:18:05 +0200

    physicsworld added coyote timers for isgrounded check

--- Engine/Physics/PhysicsWorld.cs
+++ Engine/Physics/PhysicsWorld.cs
@@ -27,6 +27,9 @@ namespace OssianForge.Engine.Physics
         public AxisLock LockPosition = AxisLock.None;
         public AxisLock LockRotation = AxisLock.None;
 
+        private readonly Dictionary<string, float> _coyoteTimers = new();
+        private const float CoyoteTime = 0.15f;
+
         public PhysicsWorld(int worldIndex, Vector3 gravity)
         {
             WorldIndex = worldIndex;
@@ -79,6 +82,9 @@ namespace OssianForge.Engine.Physics
         {
             float dt = Math.Clamp((float)delta, 0.001f, 0.033f);
 
+            foreach (var key in _coyoteTimers.Keys.ToList())
+                _coyoteTimers[key] -= dt;
+
             foreach (var body in _bodies)
                 body.SyncToJitter();
 
@@ -99,13 +105,14 @@ namespace OssianForge.Engine.Physics
             _bodies.FirstOrDefault(b => b.NodeId == nodeId);
 
 
-        public bool IsGrounded(Node node, float maxGroundAngle = 46f)
+        public bool IsGrounded(Node node, float maxGroundAngle = 90f)
         {
             var body = GetBody(node.Id);
             if (body?.JitterBody == null) return false;
 
             float cosThreshold = MathF.Cos(maxGroundAngle * MathF.PI / 180f);
             var up = new JVector(0, 1, 0);
+            bool contacted = false;
 
             foreach (Arbiter arbiter in body.JitterBody.Contacts)
             {
@@ -113,20 +120,31 @@ namespace OssianForge.Engine.Physics
 
                 JVector n0 = cd.Contact0.Normal;
                 if (n0.LengthSquared() > 0.5f && MathF.Abs((float)JVector.Dot(n0, up)) >= cosThreshold)
-                    return true;
+                { contacted = true; break; }
 
                 JVector n1 = cd.Contact1.Normal;
                 if (n1.LengthSquared() > 0.5f && MathF.Abs((float)JVector.Dot(n1, up)) >= cosThreshold)
-                    return true;
+                { contacted = true; break; }
 
                 JVector n2 = cd.Contact2.Normal;
                 if (n2.LengthSquared() > 0.5f && MathF.Abs((float)JVector.Dot(n2, up)) >= cosThreshold)
-                    return true;
+                { contacted = true; break; }
 
                 JVector n3 = cd.Contact3.Normal;
                 if (n3.LengthSquared() > 0.5f && MathF.Abs((float)JVector.Dot(n3, up)) >= cosThreshold)
-                    return true;
+                { contacted = true; break; }
             }
+
+            string id = node.Id;
+
+            if (contacted)
+            {
+                _coyoteTimers[id] = CoyoteTime;
+                return true;
+            }
+
+            if (_coyoteTimers.TryGetValue(id, out float remaining) && remaining > 0f)
+                return true;
 
             return false;
         }
